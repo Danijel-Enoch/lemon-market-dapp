a857d7591121ce612bd3364d9373588e9e27e3d4
elcharitas <jonathanirhodia@gmail.com>
2025-12-30 05:58:30 +0100

    chore: home footer revamp

--- src/components/homepage/HomepageFooter.tsx
+++ src/components/homepage/HomepageFooter.tsx
@@ -68,8 +68,8 @@ export const HomepageFooter = memo(function HomepageFooter({
 			<div className="p-6 md:px-12 md:pb-24 bg-[linear-gradient(172.34deg,#0a1a0300_-34.4%,#0a1a031c_51.7%,#1c6200_161.09%)]">
 				<div className="w-full max-w-7xl mx-auto">
 					<div className="my-20 md:my-28 h-px w-full bg-[#4dad31]/60" />
-					<div className="py-12 grid gap-8 md:grid-cols-6">
-						<div className="flex flex-col gap-4 md:col-span-3">
+					<div className="py-12 grid gap-8 grid-cols-3 md:grid-cols-6">
+						<div className="flex flex-col gap-4 col-span-3">
 							<div className="flex items-center gap-3">
 								<img
 									src={brand.logoSrc}
