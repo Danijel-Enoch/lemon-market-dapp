01f99f5351e78794f5aaab91dbeb04d5ae3ae666
elcharitas <jonathanirhodia@gmail.com>
2026-01-04 18:29:20 +0100

    refactor: Reorder Vite plugins in configuration.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+	plugins: [ tailwindcss(), reactRouter(), tsconfigPaths()],
 	css: {
 		devSourcemap: false,
 	},
