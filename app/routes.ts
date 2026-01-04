8c71e4eaf5c2e0cdf01a38b08ac3ecf85cf1a99b
elcharitas <jonathanirhodia@gmail.com>
2026-01-04 16:26:04 +0100

    refactor: remove layout function from route export

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, layout } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
 import { jamRoutes } from "react-router-jam";
 
-export default [layout("root.tsx", { id: "app-root" }, await jamRoutes())] satisfies RouteConfig;
+export default (await jamRoutes()) satisfies RouteConfig;
